323d7868351018d8f81c35c07766f4af7d658e45
shresthjindal28 <146697891+shresthjindal28@users.noreply.github.com>
2024-05-07 19:58:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Game = () => {
             let markX= true;
             let GotWinner = false;
 
+            let numberofDIsabledBox = 0;
+
             const winPattern = [
                 [0, 1, 2],
                 [0, 3, 6],
@@ -32,6 +34,7 @@ const Game = () => {
 
                     }
                     box.disabled = true;
+                    numberofDIsabledBox = numberofDIsabledBox + 1;
                     win()
                 })
 
@@ -42,22 +45,31 @@ const Game = () => {
                     const pos1 = boxes[pattern[0]].innerText;
                     const pos2 = boxes[pattern[1]].innerText;
                     const pos3 = boxes[pattern[2]].innerText;
+                    
 
 
                     if(pos1 != "" && pos2 != "" && pos3 != ""){
                         if(pos1 === pos2 && pos2 === pos3){
                             //console.log("winner");
                             displayWin.innerText = "Winner : " + pos1;
+                            GotWinner = true;
+                            boxes[pattern[0]].style.background = "green";
+                            boxes[pattern[1]].style.background = "green";
+                            boxes[pattern[2]].style.background = "green";
                             afterWinner();
+                            break;
                         }
                     }
                 }
+                if(!GotWinner && numberofDIsabledBox === 9) {
+                        console.log("DRAW...")
+                        displayWin.innerText = "Draw X - O ";
+                    }
             }
 
             const afterWinner = () => {
                 for(box of boxes){
                     box.disabled = true;
-                    GotWinner = true;
                 }
             }
 
@@ -66,28 +78,34 @@ const Game = () => {
                     if(GotWinner)
                         {
                             console.log("hi")
-                            markX= true;
-                            displayWin.innerText = "";
-                            for(box of boxes){
-                                box.disabled = false;
-                                box.innerText = "";
-                            }
+                         resetEverything();
                         }
 
                 })
             }
             restartGame();
 
+            const resetEverything = () => {
+                markX= true;
+                GotWinner = false;
+                numberofDIsabledBox = 0;
+                displayWin.innerText = "";
+                for(let pattern of winPattern){
+                    boxes[pattern[0]].style.background = "transparent";
+                    boxes[pattern[1]].style.background = "transparent";
+                    boxes[pattern[2]].style.background = "transparent";
+                }
+                for(box of boxes){
+                    box.disabled = false;
+                    box.innerText = "";
+                }
+            }
+
 
             const newGameStart = () =>{
                 newGame.addEventListener("click", () => {
-                    console.log("hi")
-                    markX= true;
-                    displayWin.innerText = "";
-                    for(box of boxes){
-                        box.disabled = false;
-                        box.innerText = "";
-                    }
+                    //console.log("hi")
+                   resetEverything();
                 })
             }
             newGameStart();
